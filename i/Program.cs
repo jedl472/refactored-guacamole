addf82c752a760ed8e61ee2d674ce219c2b935dd
Tomas Jodl <jodltomas6@gmail.com>
2025-11-30 16:02:23 +0100

    base progress

--- i/Program.cs
+++ i/Program.cs
@@ -12,13 +12,18 @@ namespace graph_utility {
 
     class Graph {
       public Dictionary<int, List<int>> edges = new Dictionary<int, List<int>>(); // list of node values connected by 
-      public List<Node> adjacement_nodes(Node originNode) { 
+      public List<Node> adjacement_nodes(Node origin_node) { 
+        List<Node> out_list = new List<Node>();
 
+        foreach (var val in edges[origin_node.value]) {
+          out_list.Add(new Node(){ value = val, predecessor = origin_node});
+        }
+        return out_list; 
       }
     }
 
     class LIFO {
-      public List<Node> queue;
+      public List<Node> queue = new List<Node>();
 
       public void add(Node node) {
         queue.Add(node);
@@ -29,26 +34,59 @@ namespace graph_utility {
 
         return node; 
       }
+
+      public bool isQueueEmpty() {
+        if (queue.Length == 0) { 
+          return true; 
+        } else { 
+          return false; 
+        }
+      }
     }
 
     class BFS {
-      Graph graph;
-      List<Node> explored;
-      LIFO queue;
-      int target_value;
+      public Graph graph;
+      List<int> explored = new List<int>();
+      LIFO queue = new LIFO(){};
+      public int target_value;
 
-      void step() {
-        foreach (Node node in graph.adjacement_nodes(queue.removeElement()))
+      public Node? step(out bool no_result) {
+        if(queue.isQueueEmpty()) { 
+          no_result = true; 
+          return null; 
+        }
+        Node origin_node = queue.removeElement();
+        Console.Write($"origin: {origin_node.value}");
+        foreach (Node node in graph.adjacement_nodes(origin_node))
         {
+          Console.Write("searching: "); Console.Write($"{node.value}, ");
           if(node.value == target_value) {
-            backtrack();
+            return node;
           } else {
-            queue.add(node);
+            if(!explored.Contains(node.value)) queue.add(node);
           }
         }
+
+        explored.Add(origin_node.value);
+
+        Console.WriteLine();
+        return null;
       }
 
-      void backtrack() {}
+      public List<int> backtrack(Node node) {
+        List<int> out_list = new List<int>() { node.value };
+        Node i_node = node;
+
+        while (i_node.predecessor != null) {
+          out_list.Add(i_node.predecessor.value);
+          i_node = i_node.predecessor;
+        }
+        return out_list;
+      }
+
+      public void init_queue(Node node) {
+        queue.queue.Add(node);
+      }
     }
 
 
@@ -83,6 +121,18 @@ namespace graph_utility {
       {
         Console.WriteLine($"{kvp.Key}: {string.Join(", ", kvp.Value)}");
       }
+     
+      bool no_result = false;
+      Node bfs_result = null;
+      BFS bfs = new BFS() { target_value = 7, graph = graph };
+      bfs.init_queue(new Node() { value = 1 });
+      while (bfs_result == null && (not no_result)) {
+        bfs_result = bfs.step(out no_result);
+      }
+
+      if (no_result == true) { Console.Write("no result found"); }
+      Console.WriteLine(string.Join(", ", bfs.backtrack(bfs_result)));
+    }
 
   }
 }
